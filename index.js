a6d0a6663e1e456f79cbd855f6d4ca85b1d55e12
Jaden Ryan <jaden.kitonga@student.moringaschool.com>
2022-06-05 17:55:02 +0300

    commit message

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift()
+}
+
+function appendCat(name){
+    let newCats=cats.slice()
+    newCats.push(name);
+    return newCats
+}
+
+function prependCat(name){
+    let newCats=cats.slice()
+    newCats.unshift(name)
+    return newCats
+}
+
+function removeLastCat(){
+    let newCats=[...cats]
+    newCats.pop();
+    return newCats
+}
+
+function removeFirstCat(){
+    let newCats=[...cats]
+    newCats.shift()
+    return newCats
+}
